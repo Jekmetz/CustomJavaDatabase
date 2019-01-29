2ebe42ca81665b0e4b3f2717949bdd839d7708e9
jekmetz <jek0025@mix.wvu.edu>
2019-01-28 19:14:06 -0500

    add wireframe for DInsert

--- src/main/java/driver/DInsert.java
+++ src/main/java/driver/DInsert.java
@@ -1,24 +1,35 @@
 package driver;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import adt.Database;
 import adt.Response;
+import adt.Table;
 
 public class DInsert implements Driver{
 	private static final Pattern pattern;
 	static {
 		pattern = Pattern.compile(
-				//INSERT\s+INTO\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)\s*\(\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*((?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s+\)\s+VALUES\s+
-				"",
+				//INSERT\s+INTO\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)\s*\(\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s*\)\s+VALUES\s*\(\s*(?<vals>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s*\)
+				"INSERT\\s+INTO\\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)\\s*\\(\\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\\s*,\\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\\s*\\)\\s+VALUES\\s*\\(\\s*(?<vals>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\\s*,\\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\\s*\\)",
 				Pattern.CASE_INSENSITIVE
 		);
 	}
 
 	@Override
 	public Response execute(Database db, String query) {
-		// TODO Auto-generated method stub
-		return null;
+		//Initialize Return variables
+		Table table = null;
+		String message = null;
+		boolean success = true;
+		
+		//Init function vars
+		Matcher matcher = pattern.matcher(query.trim());
+		
+		if(!matcher.matches()) 	return null; //If it isn't a full match, return null
+		
+		return new Response(success,message,table);
 	}
 
 }
