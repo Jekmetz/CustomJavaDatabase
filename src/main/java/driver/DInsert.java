f439b9b13e34329f0896dab5825a4884448e67ac
jekmetz <jek0025@mix.wvu.edu>
2019-02-01 11:21:09 -0500

    small change to DInsert

--- src/main/java/driver/DInsert.java
+++ src/main/java/driver/DInsert.java
@@ -1,5 +1,6 @@
 package driver;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,8 +12,8 @@ public class DInsert implements Driver{
 	private static final Pattern pattern;
 	static {
 		pattern = Pattern.compile(
-				//INSERT\s+INTO\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)\s*\(\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s*\)\s+VALUES\s*\(\s*(?<vals>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s*\)
-				"INSERT\\s+INTO\\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)\\s*\\(\\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\\s*,\\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\\s*\\)\\s+VALUES\\s*\\(\\s*(?<vals>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\\s*,\\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\\s*\\)",
+				//INSERT\s+INTO\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)(?:\s*\(\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s*\))?\s+VALUES\s*\(\s*(?<vals>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\s*,\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\s*\)
+				"INSERT\\s+INTO\\s+(?<tabName>[a-zA-Z][a-zA-Z0-9_]*)(?:\\s*\\(\\s*(?<colNames>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\\s*,\\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\\s*\\))?\\s+VALUES\\s*\\(\\s*(?<vals>[a-zA-Z][a-zA-Z0-9_]*(?:(?:\\s*,\\s*)(?:[a-zA-Z][a-zA-Z0-9_]*))*)\\s*\\)",
 				Pattern.CASE_INSENSITIVE
 		);
 	}
@@ -27,7 +28,31 @@ public class DInsert implements Driver{
 		//Init function vars
 		Matcher matcher = pattern.matcher(query.trim());
 		
-		if(!matcher.matches()) 	return null; //If it isn't a full match, return null
+		if(!matcher.matches()) 	return null; //If it isn't a full match, return null...
+		
+		if(db.containsKey(matcher.group("tabName")))	//If the table exists in the database...
+		{
+			//Init vars for corect database
+			table = db.get(matcher.group("tabName"));
+			String[] colNames = null;
+			int[] order = null;
+			
+			if(matcher.group("colNames") != null)	//If the user defined column names...
+				colNames = matcher.group("colNames").split("\\s*,\\s*");
+			else 		//If the user did not define column names
+			{
+				colNames = (String[]) table.getSchema().getStringList("column_names").toArray();
+				order = new int[colNames.length];
+				for(int i = 0; i < colNames.length; i++)
+					order[i] = i;		
+			}
+			
+		} else	//If the table does not exist in the database
+		{
+			success = false;
+			message = "The table " + matcher.group("tabName") + " does not exist in the database";
+			table = null;
+		}
 		
 		return new Response(success,message,table);
 	}
