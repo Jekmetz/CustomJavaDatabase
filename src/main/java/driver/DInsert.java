3f05a9bb999ac7efcc98dcde44c089e902501c00
jekmetz <jek0025@mix.wvu.edu>
2019-02-04 13:24:54 -0500

    Get framework for DInsert

--- src/main/java/driver/DInsert.java
+++ src/main/java/driver/DInsert.java
@@ -1,11 +1,15 @@
 package driver;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import adt.Database;
 import adt.Response;
+import adt.Row;
 import adt.Table;
 
 public class DInsert implements Driver{
@@ -19,11 +23,15 @@ public class DInsert implements Driver{
 	}
 
 	@Override
-	public Response execute(Database db, String query) {
+	public Response execute(Database db, String query) 
+	{
 		//Initialize Return variables
 		Table table = null;
 		String message = null;
 		boolean success = true;
+		
+		//error Flags
+		boolean cnDneInTable = false,dupeCn = false;
 		
 		//Init function vars
 		Matcher matcher = pattern.matcher(query.trim());
@@ -34,17 +42,50 @@ public class DInsert implements Driver{
 		{
 			//Init vars for corect database
 			table = db.get(matcher.group("tabName"));
-			String[] colNames = null;
+			String[] colNames = null, values = null;
 			int[] order = null;
 			
-			if(matcher.group("colNames") != null)	//If the user defined column names...
+			if(matcher.group("colNames") != null)	//If the user defined column names...	
+			{
 				colNames = matcher.group("colNames").split("\\s*,\\s*");
-			else 		//If the user did not define column names
+				order = new int[colNames.length];
+				
+				/*VALIDATE COLNAMES*/
+				{
+					Set<String> colNamesSet = new HashSet<String>();
+					for(int i = 0; (i < colNames.length) && !cnDneInTable && !dupeCn; i++)
+					{
+						if(table.containsKey(colNames[i]))	//Colname is valid
+						{
+							colNamesSet.add(colNames[i]);
+							if(colNamesSet.size() != i+1)	//Not a dupe
+							{
+								success = false;
+								dupeCn = true;
+							}
+						} else //Colname is not valid
+						{
+							success = false;
+							cnDneInTable = true;
+						}
+					}
+					/***********/
+				}
+			} else //If the user did not specify column names
 			{
 				colNames = (String[]) table.getSchema().getStringList("column_names").toArray();
-				order = new int[colNames.length];
-				for(int i = 0; i < colNames.length; i++)
-					order[i] = i;		
+			}
+			
+			Iterator<String> itr = table.getSchema().getStringList("column_names").iterator();
+			Row row = new Row();
+			
+			{
+				String colName;
+				while(itr.hasNext())
+				{
+					colName = itr.next();
+					
+				}
 			}
 			
 		} else	//If the table does not exist in the database
@@ -55,6 +96,22 @@ public class DInsert implements Driver{
 		}
 		
 		return new Response(success,message,table);
+	}
+	
+	private int indexOf(Object[] arr, Object obj)
+	{
+		boolean found = false;
+		int output = -1;
+		for(int i = 0; (i < arr.length) && !found; i++)
+		{
+			if(arr[i].equals(obj))
+			{
+				output = i;
+				found = true;
+			}
+		}
+		
+		return output;
 	}
 
 }
