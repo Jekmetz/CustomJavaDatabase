4196f1c4a7645f600ea6be13ca7788c912ff5df6
jekmetz <jek0025@mix.wvu.edu>
2019-03-12 09:10:29 -0400

    Add Entry class and add Hash Function

---
+++ src/main/java/adt/Entry.java
@@ -0,0 +1,38 @@
+package adt;
+
+public class Entry<K,V> implements java.util.Map.Entry<K, V> {
+	//Init Vars
+	public K key = null;
+	public V value = null;
+	
+	//Constructors
+	public Entry(K key, V value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+	
+	public Entry(K key)
+	{
+		this.key = key;
+		this.value = null;
+	}
+	
+	//Implemented methods
+	@Override
+	public K getKey() {
+		return key;
+	}
+
+	@Override
+	public V getValue() {
+		return value;
+	}
+
+	@Override
+	public V setValue(V value) {
+		this.value = value;
+		return value;
+	}
+
+}
--- src/main/java/adt/HashMap.java
+++ src/main/java/adt/HashMap.java
@@ -1,6 +1,9 @@
 package adt;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 /** 
  * This is currently just an alias for a built-in
@@ -15,6 +18,7 @@ import java.util.Map;
 public class HashMap<K,V> extends java.util.HashMap<K,V> implements Map<K,V> {
 	private static final long serialVersionUID = 1L;
 	
+	//Constructors
 	public HashMap() {
 		super();
 	}
@@ -22,4 +26,45 @@ public class HashMap<K,V> extends java.util.HashMap<K,V> implements Map<K,V> {
 	public HashMap(Map<? extends K, ? extends V> copy) {
 		super(copy);
 	}
+	
+	
+	
+	//Implemented methods
+	private static int HashCode(Object obj)
+	{
+		if(obj.getClass().getSimpleName().equals("String"))	//If it is a string...
+		{
+			String str = (String) obj;
+			Random random = null;
+			double seed = 0;
+			
+			for(int i = 0; i < str.length(); i++)
+				seed += Math.pow(str.charAt(i),str.length() - i);
+			
+			random = new Random((int)(seed % Integer.MAX_VALUE));
+			return (int) random.nextLong();
+		} else	//If it is not a string
+		{
+			return obj.hashCode();
+		}
+	}
+	
+	//Node class
+	class Node<T> {
+		private T data = null;
+		private Node<T> next = null;
+		
+		private Node(T data)
+		{
+			this.data = data;
+			this.next = null;
+		}
+		
+		private Node(T data, Node<T> ref)
+		{
+			this.data = data;
+			this.next = ref;
+		}
+	}
+
 }
