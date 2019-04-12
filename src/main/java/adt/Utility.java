39a68ece92e958390af08a99d03404be2f897b02
jekmetz <jek0025@mix.wvu.edu>
2019-04-12 12:10:24 -0400

    add getRootDirectory

--- src/main/java/adt/Utility.java
+++ src/main/java/adt/Utility.java
@@ -1,5 +1,19 @@
 package adt;
 
+import java.io.File;
+
 public class Utility {
-	public static String stripFL(String data) { return data.substring(1,data.length()-1);	}
+	public static String stripFL(String data) { return data.substring(1,data.length()-1); }
+	
+	public static File ensureDirExists(String canDirPath)
+	{
+		File file = new File(System.getProperty("user.dir") + canDirPath);
+		file.mkdir();
+		return file;
+	}
+	
+	public static File getRootDirectory(String dirName)
+	{
+		return new File(System.getProperty("user.dir") + "\\" + dirName);
+	}
 }
