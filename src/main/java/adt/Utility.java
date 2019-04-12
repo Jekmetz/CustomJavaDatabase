99ea5f71fd07863e71bb37915776a8b4bcaaea17
jekmetz <jek0025@mix.wvu.edu>
2019-04-12 10:11:52 -0400

    Add ensureDirExists in Utility's functions

--- src/main/java/adt/Utility.java
+++ src/main/java/adt/Utility.java
@@ -1,5 +1,14 @@
 package adt;
 
+import java.io.File;
+
 public class Utility {
-	public static String stripFL(String data) { return data.substring(1,data.length()-1);	}
+	public static String stripFL(String data) { return data.substring(1,data.length()-1); }
+	
+	public static File ensureDirExists(String canDirPath)
+	{
+		File file = new File(System.getProperty("user.dir") + canDirPath);
+		file.mkdir();
+		return file;
+	}
 }
