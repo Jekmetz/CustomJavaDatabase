8a12afa5edf1c7575cb18c3f4e518ec3171ad769
jekmetz <jek0025@mix.wvu.edu>
2019-04-26 12:50:00 -0400

    HASH 3 efficiency

--- src/main/java/adt/HashMap.java
+++ src/main/java/adt/HashMap.java
@@ -445,39 +445,6 @@ public class HashMap<K,V> implements Map<K,V>{
 		}
 	}
 	
-	/*
-	 *  Node<K,V> next;        // next entry to return
-        Node<K,V> current;     // current entry
-        int expectedModCount;  // for fast-fail
-        int index;             // current slot
-
-        HashIterator() {
-            expectedModCount = modCount;
-            Node<K,V>[] t = table;
-            current = next = null;
-            index = 0;
-            if (t != null && size > 0) { // advance to first entry
-                do {} while (index < t.length && (next = t[index++]) == null);
-            }
-        }
-
-        public final boolean hasNext() {
-            return next != null;
-        }
-
-        final Node<K,V> nextNode() {
-            Node<K,V>[] t;
-            Node<K,V> e = next;
-            if (modCount != expectedModCount)
-                throw new ConcurrentModificationException();
-            if (e == null)
-                throw new NoSuchElementException();
-            if ((next = (current = e).next) == null && (t = table) != null) {
-                do {} while (index < t.length && (next = t[index++]) == null);
-            }
-            return e;
-	 */
-	
 	//Entry Class
 	class MapEntry<A,B> implements java.util.Map.Entry<A, B> {
 		//Init Vars
