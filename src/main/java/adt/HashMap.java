f5ae04504beb18c3c394ae4cec780d2944e79779
jekmetz <jek0025@mix.wvu.edu>
2019-04-26 10:07:32 -0400

    Goodbye, casting hell!

--- src/main/java/adt/HashMap.java
+++ src/main/java/adt/HashMap.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-
 /** 
  * This is currently just an alias for a built-in
  * implementation of a hash-based Map and is therefore
@@ -24,7 +23,7 @@ public class HashMap<K,V> implements Map<K,V>{
 	//private static final long serialVersionUID = 1L;
 	private final int INITIAL_SIZE = 11;	//Initial size of Map
 	private int size = 0, alpha = 0;		//Hard coded size and Alpha
-	private Object[] data = null;			//data object
+	private Node<MapEntry<K,V>>[] data = null;			//data object
 	private int[] arrLengths = {23,59,127,257,521,1049,2111,4229,8461,16879,33023,66271,104729,208049,416159,832189,1299827};
 	private int arrLengthIndex = 0;			//index deciding what prime resize length we are on
 	boolean doAResizeMate = false;			//flag determining whether or not to resize
@@ -32,8 +31,9 @@ public class HashMap<K,V> implements Map<K,V>{
 	/**
 	 * Constructor that creates new Node array with size INITIAL_SIZE
 	 */
+	@SuppressWarnings("unchecked")
 	public HashMap() {
-		data = new Object[INITIAL_SIZE];
+		data = (Node<MapEntry<K, V>>[])new Node[INITIAL_SIZE];
 		for(int i = 0; i < data.length; i++)							//For every element in data...
 			data[i] = new Node<MapEntry<K,V>>(new MapEntry<K,V>(null));	//Add a head node to it.
 		
@@ -58,16 +58,15 @@ public class HashMap<K,V> implements Map<K,V>{
 	public boolean isEmpty() { return size == 0;}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public boolean containsKey(Object key) {
-		Node<MapEntry<K,V>> head = (Node<MapEntry<K,V>>)data[Math.floorMod(localHash(key),data.length)];	//Head is the data element that will be accepting the new Entry
+		Node<MapEntry<K,V>> head = data[Math.floorMod(localHash(key),data.length)];	//Head is the data element that will be accepting the new Entry
 		Node<MapEntry<K,V>> cur = null;	//Initialize cur
 		boolean found = false;			//flag to check if the key has been found
 		
 		cur = head.next;				//set cur to the next element past the head
 		while(( cur != null ) && ( !found ))	//while we are not at the end of the list and we have not found it...
 		{
-			if(((MapEntry<K,V>)cur.data).getKey().equals(key))	//If the key is in that position...
+			if(cur.data.getKey().equals(key))	//If the key is in that position...
 				found = true;
 			
 			cur = cur.next;	//increment the cur. If it was found, then it will be the one after the found key but that's okay because we don't have to use it!
@@ -77,7 +76,6 @@ public class HashMap<K,V> implements Map<K,V>{
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public boolean containsValue(Object value) {
 		boolean found = false;
 		
@@ -88,12 +86,12 @@ public class HashMap<K,V> implements Map<K,V>{
 				Node<MapEntry<K,V>> cur = null;
 				for(int i = 0; ( i < data.length ) && ( !found ); i++)	//for all of the node heads....
 				{
-					head = (Node<MapEntry<K,V>>) data[i];
+					head = data[i];
 					cur = head.next;
 					
 					while(( cur != null ) && ( !found ))
 					{
-						if(((MapEntry<K,V>)cur.data).getValue().equals(value))
+						if(cur.data.getValue().equals(value))
 							found = true;
 						cur = cur.next;
 					}
@@ -105,9 +103,8 @@ public class HashMap<K,V> implements Map<K,V>{
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public V get(Object key) {
-		Node<MapEntry<K,V>> head = (Node<MapEntry<K,V>>) data[Math.floorMod(localHash(key),data.length)]; //set the head to the element in which that object will fall...
+		Node<MapEntry<K,V>> head =data[Math.floorMod(localHash(key),data.length)]; //set the head to the element in which that object will fall...
 		Node<MapEntry<K,V>> cur;
 		boolean found = false;
 		V output = null;
@@ -117,9 +114,9 @@ public class HashMap<K,V> implements Map<K,V>{
 			cur = head.next;
 			while(( cur != null ) && ( !found ))	//for all the items in the list and while it's not found...
 			{
-				if(((MapEntry<K, V>)cur.data).getKey().equals(key))	//If we have found the node that has this key...
+				if(cur.data.getKey().equals(key))	//If we have found the node that has this key...
 				{
-					output = ((MapEntry<K, V>)cur.data).getValue();	//give it to me!
+					output = cur.data.getValue();	//give it to me!
 					found = true;									//Stop the loop while you're at it!
 				}
 				
@@ -131,7 +128,6 @@ public class HashMap<K,V> implements Map<K,V>{
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public V put(K key, V value) {
 		
 		if(doAResizeMate)	//If we should do a resize...
@@ -140,7 +136,7 @@ public class HashMap<K,V> implements Map<K,V>{
 		V output = this.get(key);	//Outputs the old value
 		boolean update = false;		//is it an update?
 		
-		Node<MapEntry<K,V>> head = (Node<MapEntry<K,V>>) data[Math.floorMod(localHash(key),data.length)];
+		Node<MapEntry<K,V>> head = data[Math.floorMod(localHash(key),data.length)];
 		Node<MapEntry<K,V>> cur = head;
 		
 		while(cur.next != null && !update)	//traverse the list until we are adding a node to the cur.next or we have determined that it is an update!
@@ -162,25 +158,24 @@ public class HashMap<K,V> implements Map<K,V>{
 			size++;
 		}
 		
-		doAResizeMate = calculateAlpha() >= 5; //Calculate alpha every time
+		doAResizeMate = calculateAlpha() >= 10; //Calculate alpha every time
 			
 		return output;
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public V remove(Object key) {
 		V value = null;
 		Node<MapEntry<K,V>> kill = null;
 		if(this.containsKey(key)) //If this contains the key to remove...
 		{
-			Node<MapEntry<K,V>> head = (Node<MapEntry<K,V>>) data[Math.floorMod(localHash(key),data.length)];
+			Node<MapEntry<K,V>> head = data[Math.floorMod(localHash(key),data.length)];
 			Node<MapEntry<K,V>> cur = head;
 			
-			while(!((MapEntry<K, V>)cur.next.data).getKey().equals(key))	//traverse the list until we are just before the one to remove
+			while(!cur.next.data.getKey().equals(key))	//traverse the list until we are just before the one to remove
 				cur = cur.next;
 			
-			value = ((MapEntry<K, V>)cur.next.data).getValue();
+			value = cur.next.data.getValue();
 			
 			kill = cur.next;			//That is the one we want to obliterate
 			cur.next = cur.next.next;	//set the curent node.next to the one after the removal one
@@ -197,9 +192,10 @@ public class HashMap<K,V> implements Map<K,V>{
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void clear() {
-		data = new Object[INITIAL_SIZE];
+		data = new Node[INITIAL_SIZE];
 		for(int i = 0; i < data.length; i++)	//Just replace everything with a head
 			data[i] = new Node<MapEntry<K,V>>(new MapEntry<K,V>(null));
 		
@@ -304,8 +300,8 @@ public class HashMap<K,V> implements Map<K,V>{
 	private void resize()
 	{
 		doAResizeMate = false;	//Set this global variable to false to not do another resize until next time
-		Object[] temp = data;
-		data = (arrLengthIndex < arrLengths.length) ? new Object[arrLengths[arrLengthIndex]] : new Object[data.length*2+1];
+		Node<MapEntry<K,V>>[] temp = data;
+		data = (arrLengthIndex < arrLengths.length) ? new Node[arrLengths[arrLengthIndex]] : new Node[data.length*2+1];
 		arrLengthIndex++;
 		
 		//initialize data again...
@@ -319,7 +315,7 @@ public class HashMap<K,V> implements Map<K,V>{
 			Node<MapEntry<K,V>> cur = null;
 			for(int i = 0; i < temp.length; i++) 		//for every chain in the array...
 			{
-				cur = (Node<MapEntry<K, V>>) temp[i];	//cur is the next head node
+				cur = temp[i];	//cur is the next head node
 				
 				while (cur.next != null)	//for every element in the chain...
 				{
@@ -333,7 +329,6 @@ public class HashMap<K,V> implements Map<K,V>{
 	public int getAlpha() { return alpha; }
 	
 	//Object Overrides
-	@SuppressWarnings("unchecked")
 	@Override
 	public String toString()
 	{
@@ -344,7 +339,7 @@ public class HashMap<K,V> implements Map<K,V>{
 		
 		for(int i = 0; i < data.length; i++)		//for all the indexes in data
 		{
-			head = (Node<MapEntry<K, V>>) data[i];	//set the head for that element
+			head = data[i];	//set the head for that element
 			cur = head.next;						//set cur to the first good one
 			
 			if(cur != null)	//If the first good element is not null...
@@ -375,18 +370,7 @@ public class HashMap<K,V> implements Map<K,V>{
 			adt.HashMap<K, V> copy = (adt.HashMap<K,V>)a; //If it has made it here, then it is an adt.HashMap
 			
 			if(this.size() == copy.size())	//If the sizes are the same...
-			{
-				ViewIterator<MapEntry<K,V>> thisItr = this.viewIterator();
-				ViewIterator<MapEntry<K,V>> copyItr = copy.viewIterator();
-				
-				output = true; //set output to true until proven false at this point
-				
-				while (thisItr.hasNext() && output)
-				{
-					output = output && thisItr.next().equals(copyItr.next());	//make sure that all of the entries are the same
-				}
-				
-			}
+				output = this.entrySet().equals(copy.entrySet());				
 		}
 		
 		return output;
@@ -401,18 +385,6 @@ public class HashMap<K,V> implements Map<K,V>{
 			output += e.hashCode();
 		}
 		return output;
-	}
-	
-	//Helpers
-	private ViewIterator<MapEntry<K,V>> viewIterator()
-	{
-		return new ViewIterator<MapEntry<K,V>>() 
-		{
-			public MapEntry<K,V> next()
-			{
-				return nextEntry();
-			}
-		};
 	}
 	
 	//Node class
@@ -442,15 +414,15 @@ public class HashMap<K,V> implements Map<K,V>{
 	private abstract class ViewIterator<T> implements Iterator<T> {
 		int index = 0;
 		int itemsViewed = 0;
-		@SuppressWarnings("unchecked")
-		Node<MapEntry<K,V>> cur = (HashMap<K, V>.Node<MapEntry<K, V>>) data[0];
+		Node<MapEntry<K,V>> cur = data[0];
+		Node<MapEntry<K,V>> next = null;
 		
 		@Override
 		public boolean hasNext() {
+			//return next != null;
 			return itemsViewed < HashMap.this.size();
 		}
 		
-		@SuppressWarnings("unchecked")
 		protected MapEntry<K,V> nextEntry() {
 			MapEntry<K,V> output = null;
 			if(index < data.length)	//If we have the ability to search
@@ -459,16 +431,10 @@ public class HashMap<K,V> implements Map<K,V>{
 					cur = cur.next;	
 				else 					//If we need to move on to the next chain
 				{
-					boolean found = false;
-					while(!found) 	//loop as long as it is not found
-					{
+					index++;
+					while(data[index].next == null)
 						index++;
-						if(((Node<MapEntry<K,V>>)data[index]).next != null)
-						{
-							found = true;
-							cur = ((Node<MapEntry<K,V>>)data[index]).next;
-						}
-					}
+					cur = data[index].next;
 				}
 				output = cur.data;
 			} else 	//If we do not have the ability to search
@@ -478,6 +444,39 @@ public class HashMap<K,V> implements Map<K,V>{
 			return output;
 		}
 	}
+	
+	/*
+	 *  Node<K,V> next;        // next entry to return
+        Node<K,V> current;     // current entry
+        int expectedModCount;  // for fast-fail
+        int index;             // current slot
+
+        HashIterator() {
+            expectedModCount = modCount;
+            Node<K,V>[] t = table;
+            current = next = null;
+            index = 0;
+            if (t != null && size > 0) { // advance to first entry
+                do {} while (index < t.length && (next = t[index++]) == null);
+            }
+        }
+
+        public final boolean hasNext() {
+            return next != null;
+        }
+
+        final Node<K,V> nextNode() {
+            Node<K,V>[] t;
+            Node<K,V> e = next;
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+            if (e == null)
+                throw new NoSuchElementException();
+            if ((next = (current = e).next) == null && (t = table) != null) {
+                do {} while (index < t.length && (next = t[index++]) == null);
+            }
+            return e;
+	 */
 	
 	//Entry Class
 	class MapEntry<A,B> implements java.util.Map.Entry<A, B> {
